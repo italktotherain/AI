68bb36c4b022a23376414888bedd47c7b17172ee
italktotherain <selena.d.grant@gmail.com>
2015-02-20 15:07:05 -0500

    Update DFS.m
    Should be a full implementation now
    + took code from Austen's BFS and used as base because it already had needed data structures and methods implemented
    - changed the generation of children to be more spelled out (just because that's how I prefer my code sometimes)
    + added the recursive call to DFS within each child generation method
        + this should (logically) mean that it will prioritize left to right the children (the consistent order having been dictated by the positions of validMoves) and therefore should be a valid DFS implementation

--- DFS.m
+++ DFS.m
@@ -1,8 +1,8 @@
-function [runtime] = DFS(P) % iterative, the lack of FIFO queue makes recursive difficult
+function [runtime] = DFS(current, runtime, S) % recursive
 tic
 runtime = 0; % good reference point for if finished yet
 Puzzle = [1 2 3 4 5 6 7 8 9];
-if (isSolvable(Puzzle) == 0)
+if (isSolvable(current) == 0)
     disp('INVALID PUZZLE');
     return
 end
@@ -14,7 +14,116 @@ end
 
 % using http://en.wikipedia.org/wiki/Depth-first_search psuedocode as reference
 
+goalState = checkState(current);
+if (goalState == 1)
+    runtime = toc;
+    disp(runtime);
+    disp(current);
+    return
+end
+blankIndex = findBlank(current);
+validMoves = findValidMoves(blankIndex);
+sLength = numel(S)/9;
+isInS =0;
+child = current;
+if (validMoves(1) == 1)
+    % number to go in the blank spot is at the array position 3 previous
+    newNum = child(blankIndex - 3);
+    % assign the number to old blank spot
+    child(blankIndex) = newNum;
+    % assign the old spot with the number to be blank, or 9
+    child(blankIndex - 3) = 9;
 
+    for (i = 1:sLength)
+        if((S(i,:) == child) == 9)
+           isInS = 1; 
+        end
+    end 
+    if (isInS  == 0)
+        S(end+1, :) = child;
+        % child is not in S, so it has not been checked yet
+        % recursively check it
+        runtime =  DFS(child, runtime, S);
+        if (runtime != 0) % solution was found, return it
+            % no disp() commands here, they are at the top in the upper layer
+            return
+        end
+    end
+    isInS = 0;
+end
+
+if (validMoves(2) == 1)
+    % number to go in the blank spot is at the array position 3 after
+    newNum = child(blankIndex + 3);
+    % assign the number to old blank spot
+    child(blankIndex) = newNum;
+    % assign the old spot with the number to be blank, or 9
+    child(blankIndex + 3) = 9;
+    for (i = 1:sLength)
+        if((S(i,:) == child) == 9)
+           isInS = 1; 
+        end
+    end 
+    if (isInS  == 0)
+        S(end+1, :) = child;
+        % child is not in S, so it has not been checked yet
+        % recursively check it
+        runtime =  DFS(child, runtime, S);
+        if (runtime != 0) % solution was found, return it
+            return
+        end
+    end
+    isInS = 0;
+end
+    
+if (validMoves(3) == 1)
+    % number to go in the blank spot is at the array position 1 previous
+    newNum = child(blankIndex - 1);
+    % assign the number to old blank spot
+    child(blankIndex) = newNum;
+    % assign the old spot with the number to be blank, or 9
+    child(blankIndex - 1) = 9;
+    for (i = 1:sLength)
+        if((S(i,:) == child) == 9)
+           isInS = 1; 
+        end
+    end 
+    if (isInS  == 0)
+        S(end+1, :) = child;
+        % child is not in S, so it has not been checked yet
+        % recursively check it
+        runtime =  DFS(child, runtime, S);
+        if (runtime != 0) % solution was found, return it
+            return
+        end
+    end
+    isInS = 0;
+end
+    
+if (validMoves(4) == 1)
+    % number to go in the blank spot is at the array position 1 after
+    newNum = child(blankIndex + 1);
+    % assign the number to old blank spot
+    child(blankIndex) = newNum;
+    % assign the old spot with the number to be blank, or 9
+    child(blankIndex + 1) = 9;
+    for (i = 1:sLength)
+        if((S(i,:) == child) == 9)
+           isInS = 1; 
+        end
+    end 
+    if (isInS  == 0)
+        S(end+1, :) = child;
+        % child is not in S, so it has not been checked yet
+        % recursively check it
+        runtime =  DFS(child, runtime, S);
+        if (runtime != 0) % solution was found, return it
+            return
+        end
+    end
+    isInS = 0;
+end
+% if it gets to this point, then the algorithm wasn't solvable, so it should never actually get here
 
 
 function [queue] = enqueue(queue, value) 
